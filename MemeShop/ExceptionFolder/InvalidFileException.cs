197f4e5c6ad7799c16e4e97c86479b08e68723f1
noln <mark3dom@mail.ru>
2019-03-26 19:06:16 +0300

    type commit

---
+++ MemeShop/ExceptionFolder/InvalidFileException.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MemeShop.ExceptionFolder
+{
+    public class InvalidFileException : Exception
+    {
+        public string Property { get; set; }
+        public InvalidFileException(string message, string prop) : base(message)
+        {
+            Property = prop;
+        }
+    }
+}
--- MemeShop/Util Classes/AdminUIHelper.cs
+++ MemeShop/Util Classes/AdminUIHelper.cs
@@ -24,41 +24,6 @@ namespace MemeShop.Controllers.Admin
             return createMap();
         }
 
-        public ShopItemViewModel GetCurrentUser(int? id)
-        {
-            return findInDB(id);
-        }
-
-        public string GetFullserverPath(int id)
-        {
-            return getPath(id);
-        }
-
-        public void DeleteImageOnServer(string serverpath)
-        {
-            deleteFromServer(serverpath);
-        }
-
-        public void DeleteItemFromServer(int id)
-        {
-            deleteItem(id);
-        }
-
-        public void EditItemOnServer(ShopItemViewModel context)
-        {
-            editItem(context);
-        }
-
-        public string GetFullImageName(HttpPostedFileBase image)
-        {
-            return getImageName(image);
-        }
-
-        public void CreateItemOnServer(ShopItemViewModel context, string modelPath)
-        {
-            createItem(context, modelPath);
-        }
-
         private List<ShopItemViewModel> createMap()
         {
             IEnumerable<DTOShopItem> shop = itemService.GetAll();
@@ -66,16 +31,32 @@ namespace MemeShop.Controllers.Admin
             return mapper.Map<IEnumerable<DTOShopItem>, List<ShopItemViewModel>>(shop);
         }
 
+        public ShopItemViewModel GetCurrentUser(int? id)
+        {
+            return findInDB(id);
+        }
+
         private ShopItemViewModel findInDB(int? id)
         {
             var context = itemService.Get(id);
-            return new ShopItemViewModel { Name = context.Name, Description = context.Description, Price = context.Price, PhotoPath = context.PhotoPath };
+            return new ShopItemViewModel { Name = context.Name, Description = context.Description,
+                                           Price = context.Price, PhotoPath = context.PhotoPath };
+        }
+
+        public string GetFullserverPath(int id)
+        {
+            return getPath(id);
         }
 
         private string getPath(int id)
         {
             var model = itemService.Get(id);
             return model.PhotoPath;
+        }
+
+        public void DeleteImageOnServer(string serverpath)
+        {
+            deleteFromServer(serverpath);
         }
 
         private void deleteFromServer(string serverpath)
@@ -90,34 +71,65 @@ namespace MemeShop.Controllers.Admin
             }
         }
 
+        public void DeleteItemFromServer(int id)
+        {
+            deleteItem(id);
+        }
+
         private void deleteItem(int id)
         {
             itemService.Delete(id);
         }
 
+        public void EditItemOnServer(ShopItemViewModel context)
+        {
+            editItem(context);
+        }
+
         private void editItem(ShopItemViewModel context)
         {
-            DTOShopItem model = new DTOShopItem { Id = context.Id, Name = context.Name, Description = context.Description, Price = context.Price, PhotoPath = context.PhotoPath };
+            DTOShopItem model = new DTOShopItem { Id = context.Id, Name = context.Name,
+                                                  Description = context.Description, Price = context.Price,
+                                                  PhotoPath = context.PhotoPath };
             itemService.Edit(model);
+        }
+
+        public string GetFullImageName(HttpPostedFileBase image)
+        {
+            return getImageName(image);
         }
 
         private string getImageName(HttpPostedFileBase image)
         {
+            string[] expansions = { ".tiff", ".jpeg", ".bmp", ".jpe", ".jpg", ".png", ".gif", ".psd", };
             try
             {
-                return Path.GetFileName(image.FileName);
+                string imgName = Path.GetFileName(image.FileName);
+                foreach (var item in expansions)
+                {
+                    if (imgName.EndsWith(item))
+                        return imgName;
+                }
+                throw new InvalidFileException("File not an image", "");
             }
-            catch (EmptyFileException)
+            catch(NullReferenceException)
             {
                 throw new EmptyFileException("File not found", "");
             }
         }
 
+        public void CreateItemOnServer(ShopItemViewModel context, string modelPath)
+        {
+            createItem(context, modelPath);
+        }
+        
         private void createItem(ShopItemViewModel context, string modelPath)
         {
             try
             {
-                DTOShopItem model = new DTOShopItem { Name = context.Name, Description = context.Description, Price = context.Price, PhotoPath = modelPath };
+                DTOShopItem model = new DTOShopItem { Name = context.Name,
+                                                      Description = context.Description,
+                                                      Price = context.Price, PhotoPath = modelPath };
                 itemService.Create(model);
             }
             catch (Exception)
